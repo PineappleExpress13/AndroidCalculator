80efa0a9880c1ff9170a3dd08e0748a977cf267b
PineappleExpress13 <josupirodriguez@gmail.com>
2016-12-05 18:18:26 +0100

    Inicio del proyecto.

---
+++ app/src/main/java/com/example/pineapple/calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+package com.example.pineapple.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Button;
+import java.lang.Object;
+import java.lang.Math;
+
+public class Calculadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+    }
+}
